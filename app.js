259c778d84a16ef2c61f916b93b1dbd709adaa7f
Vivek Chandra Sardar <chandravivek52@gmail.com>
2024-01-28 19:47:53 +0530

    create pug-template...

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const port = 3003;
 
 const server = app.listen(port, () => console.log("Server listening on port " + port));
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+// app.get("/", (req, res, next) => {
+//     res.status(200).send("Yahoo!");
+// });
+
 app.get("/", (req, res, next) => {
-    res.status(200).send("Yahoo!");
-});
+    res.status(200).render("home");
+})
